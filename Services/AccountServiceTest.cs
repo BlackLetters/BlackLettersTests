3d21d47827fd41c44a7e6ed0499168dc7994e39d
CristiPaul <32931823+CristiPaul@users.noreply.github.com>
2020-05-21 19:31:09 +0300

    Add files via upload

---
+++ Services/AccountServiceTest.cs
@@ -0,0 +1,76 @@
+﻿using Biblioteca.Models;
+using Biblioteca.Services.ImplementationServices;
+using Biblioteca.Services.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteca_LogicTest
+{
+    [TestClass]
+    public class AccountServiceTest
+    {
+        private Mock<AccountService> accountServiceMock = new Mock<AccountService>();
+        private Mock<IRepositoryWrapper> accountRepoMock = new Mock<IRepositoryWrapper>();
+        private int id = 2;
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            var account = new Account
+            {
+                ID = 2,
+                Name = "ceva",
+                Email = "ceva@ceva.com",
+                Password = "ceva2", 
+                Username = "ceva1",
+                UserRole = "brb",
+            };
+
+            accountRepoMock.Setup(teacherRepo => teacherRepo.Save());
+        }
+
+        [TestMethod]
+        public void GetAllAccountsTest()
+        {
+            var accountService = new AccountService(accountRepoMock.Object);
+
+            var badAccount = "sadjshd";
+
+            Assert.AreEqual(accountService, badAccount);
+        }
+
+        [TestMethod]
+        public void GetDetailsByIdTest()
+        {
+            var accountService = new AccountService(accountRepoMock.Object);
+
+            var badID = " adas";
+
+            Assert.AreEqual(accountService, badID);
+        }
+
+        [TestMethod]
+        public void CreateTest()
+        {
+            var accountService = new AccountService(accountRepoMock.Object);
+
+            var badName = "testbad";
+
+            Assert.AreEqual(accountService, badName);
+        }
+
+        [TestMethod]
+        public void ExitsTest()
+        {
+            var accountService = new AccountService(accountRepoMock.Object);
+
+            var flag = true;
+
+            Assert.AreEqual(accountService, flag);
+        }
+
+    }
+}
---
+++ Services/BookServiceTest.cs
@@ -0,0 +1,75 @@
+﻿using Biblioteca.Models;
+using Biblioteca.Services.ImplementationServices;
+using Biblioteca.Services.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteca_LogicTest.Services
+{
+    [TestClass]
+    public class BookServiceTest
+    {
+        private Mock<IRepositoryWrapper> bookRepoMock = new Mock<IRepositoryWrapper>();
+        private int id = 2;
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            var book = new Book
+            {
+                ID = 2,
+                Title = "ceva",
+                Price = "20",
+                Status = "yes",
+                Type = "ceva1",
+                Author = "brb",
+            };
+
+            bookRepoMock.Setup(teacherRepo => teacherRepo.Save());
+        }
+
+        [TestMethod]
+        public void GetAllBooksTest()
+        {
+            var bookService = new BookService(bookRepoMock.Object);
+
+            var badBook = "sadjshd";
+
+            Assert.AreEqual(bookService, badBook);
+        }
+
+        [TestMethod]
+        public void GetDetailsByIdTest()
+        {
+            var bookService = new BookService(bookRepoMock.Object);
+
+            var badID = " adas";
+
+            Assert.AreEqual(bookService, badID);
+        }
+
+        [TestMethod]
+        public void CreateTest()
+        {
+            var bookService = new BookService(bookRepoMock.Object);
+
+            var badName = "testbad";
+
+            Assert.AreEqual(bookService, badName);
+        }
+
+        [TestMethod]
+        public void ExitsTest()
+        {
+            var bookService = new BookService(bookRepoMock.Object);
+
+            var flag = true;
+
+            Assert.AreEqual(bookService, flag);
+        }
+
+    }
+}
